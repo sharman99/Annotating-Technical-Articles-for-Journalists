997d55bcd3d6c816629a827855f6b34568e2d841
Charlie Curnin <charliecurnin@Charlies-MacBook-Pro.local>
2021-03-15 20:47:27 -0400

    add text to spinner

--- src/constants.js
+++ src/constants.js
@@ -4,12 +4,18 @@ import Loader from "react-loader-spinner";
 export const LOADING_TEXT = "Loading...";
 
 export const SPINNER = (
-  <Loader
-    type="Bars"
-    color="#FE6847"
-    height={30}
-    width={200}
-  />
+  <React.Fragment>
+    <Loader
+      type="Bars"
+      color="#FE6847"
+      height={30}
+      width={200}
+    />
+    <div className="please-wait">
+      <span className="italic">Summarizing can be hard </span>😅.
+      <span className="italic"> Please hold on.</span>
+    </div>
+  </React.Fragment>
 );
 
 export const ERROR_TEXT = "Something went wrong, sorry.";
